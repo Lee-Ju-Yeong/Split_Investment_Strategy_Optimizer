2e9b09b15ec751129ca0f774554ca0f4215096a2
Your Name <youremail@example.com>
2024-07-21 20:27:20 +0900

    saving point1 for parameter_tunning

--- parameter_simulation_runner.py
+++ parameter_simulation_runner.py
@@ -4,10 +4,14 @@ from tqdm import tqdm
 import pandas as pd
 from datetime import datetime
 import configparser
+import numpy as np
 
-# 백테스팅과 관련된 함수들 가져오기
+# MagicSplit_Backtesting_Optimizer 모듈의 함수와 클래스 가져오기
 from MagicSplit_Backtesting_Optimizer import (
-    portfolio_backtesting, calculate_mdd
+    Position, Trade, get_stock_codes, load_stock_data_from_mysql, calculate_additional_buy_drop_rate,
+    calculate_sell_profit_rate, initial_buy_sell, additional_buy, additional_sell, get_trading_dates_from_db,
+    calculate_mdd, portfolio_backtesting, plot_backtesting_results, check_if_already_calculated, 
+    run_backtesting_for_period, calculate_average_results, average_cagr_wrapper, backtesting_wrapper
 )
 
 # 설정 파일 읽기
@@ -34,18 +38,19 @@ per_threshold = 10
 pbr_threshold = 1
 div_threshold = 1.0
 min_additional_buy_drop_rate = 0.005
+
 # 백테스팅 파라미터 옵션 설정
 num_splits_options = [20]
 buy_threshold_options = [30]
-investment_ratio_options = [0.3, 0.25]
+investment_ratio_options = [0.3, ]#0.25
 consider_delisting_options = [False]
-max_stocks_options = [40, 50, 55]
+max_stocks_options = [40,] # 50, 55]
 
 # 파라미터 조합 생성
 combinations = [(n, b, i, c, m) for n in num_splits_options for b in buy_threshold_options for i in investment_ratio_options for c in consider_delisting_options for m in max_stocks_options]
 
 # 여러 기간 설정
-time_periods = [(2006, 2023), (2008, 2023), (2010, 2023), (2012, 2023), (2014, 2023)]
+time_periods = [(2006, 2008), ] #(2008, 2023), (2010, 2023), (2012, 2023), (2014, 2023)
 
 # 파일 저장 경로 설정
 current_time_str = datetime.now().strftime('%Y%m%d_%H%M%S')
@@ -56,34 +61,33 @@ if not os.path.exists(results_folder):
 # 파라미터 조합 엑셀 파일 저장 경로
 results_file = os.path.join(results_folder, f'parameter_combinations_{current_time_str}.csv')
 
-# 백테스팅 실행 및 결과 저장
-def backtesting_wrapper(params):
-    num_splits, buy_threshold, investment_ratio, consider_delisting, max_stocks = params
-    try:
-        _, _, portfolio_values_over_time, _, _, _, _, cagr = portfolio_backtesting(
-            initial_capital, num_splits, investment_ratio, buy_threshold, start_date, end_date, db_params, 
-            per_threshold, pbr_threshold, div_threshold, min_additional_buy_drop_rate, consider_delisting, max_stocks
-        )
-        mdd = calculate_mdd(portfolio_values_over_time)
-        return num_splits, buy_threshold, investment_ratio, consider_delisting, max_stocks, cagr, mdd
-    except Exception as e:
-        print(f'Error in backtesting: {e}')
-        return num_splits, buy_threshold, investment_ratio, consider_delisting, max_stocks, None, None
-
 def run_backtesting_and_save_results():
     with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
         futures = []
         for param in combinations:
-            futures.append(executor.submit(backtesting_wrapper, param))
-        
+            if not check_if_already_calculated(param[0], param[1], param[2], param[3], param[4], results_folder):
+                futures.append(executor.submit(
+    average_cagr_wrapper, 
+    param, 
+    time_periods, 
+    db_params, 
+    per_threshold, 
+    pbr_threshold, 
+    div_threshold, 
+    min_additional_buy_drop_rate, 
+    param[3], 
+    param[4], 
+    results_folder
+))
         results = []
         for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures)):
             result = future.result()
             results.append(result)
 
-    results_df = pd.DataFrame(results, columns=["num_splits", "buy_threshold", "investment_ratio", "consider_delisting", "max_stocks", "CAGR", "MDD"])
+    results_df = pd.DataFrame(results, columns=["num_splits", "buy_threshold", "investment_ratio", "consider_delisting", "max_stocks", "Average_Total_Value", "Average_CAGR", "Average_MDD"])
     results_df.to_csv(results_file, index=False)
-    print(results_df.sort_values(by="CAGR", ascending=False))
+    print(results_df.sort_values(by="Average_CAGR", ascending=False))
+
 
 if __name__ == "__main__":
     run_backtesting_and_save_results()
